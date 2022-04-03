d8f5abdc34a64182f6ebafc45c6ddc33977f4ed9
Squeezedcoma <michael.wayner@bryanuniversity.edu>
2022-04-02 23:56:19 -0400

    week2

--- week2/assignments/calendar/calendar.js
+++ week2/assignments/calendar/calendar.js
@@ -6,10 +6,26 @@
 
 // modify this script to use the first day of the month the user selects in place of the const today 
 
-const today = new Date('5/1/2021')
-const month = today.getMonth()
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var select = document.getElementById("select");
+
+let options = months.map((month, index) => {
+   select.innerHTML +=  `<option value= ${index}>${month}</option>`
+})
+
+document.getElementById("button").addEventListener("click", function() {
+    printCalendar()
+})
+function printCalendar() {
+
+const month = document.getElementById("select").value 
+const yearValue = document.getElementById('yearInput').value
+
+document.getElementById('calendarDays').innerHTML = ''
+const calDate = `${document.getElementById('select').value}  ${document.getElementById('yearInput').value}`
+const firstDay = new Date(yearValue, month, 1)
 let days
-switch (month) {
+switch (Number(month)) {
     case 1:
         days = 28
         break
@@ -24,7 +40,8 @@ switch (month) {
 }
     
 let x
-const weekday = today.getDay()
+const weekday = firstDay.getDay() 
+console.log(weekday)
 for (x = 0; x < weekday; x++){
     document.getElementById('calendarDays').innerHTML += "<div class='blankDay'>&nbsp;</div>"
 }
@@ -35,13 +52,15 @@ do {
     document.getElementById('calendarDays').innerHTML += `<div class='calendarCell'>${dt}</div`
 } while ( dt < days)
 
-const monthName = today.toLocaleDateString('default', {month:'long'})
-const year = today.getFullYear()
+const monthName = firstDay.toLocaleDateString('default', {month:'long'})
+const year = firstDay.getFullYear()
 document.querySelector('.calendarTitle').innerText = `${monthName} ${year}`
 
-const remainder = (7 - ((x + dt) % 7))
+
+const remainder = (7 - ((x + dt) % 7)) 
 let y = 0
 while ( y < remainder) {
     document.getElementById('calendarDays').innerHTML += "<div class='blankDay'>&nbsp;</div>"
     y++
+}
 }
