55581bb7b557a50496533c811468a318f7e4607f
Squeezedcoma <michael.wayner@bryanuniversity.edu>
2022-04-10 21:58:56 -0400

    week3

--- week3/assignments/starwars/starwars.js
+++ week3/assignments/starwars/starwars.js
@@ -1,25 +1,44 @@
 // declare any necessary variables
+let queryType, itemID;
 
 // define a function called 'fetchData()' that passes the values from 
 // the 'queryType' and 'itemID' elements in starwars.html to the function 
 // called 'getFromSWAPI()'
 
+
+function fetchData() {
+    queryType = document.querySelector("#queryType").value;
+    itemID = document.querySelector("#itemID").value;
+    getFromSWAPI(queryType, itemID);
+}
+
+
 function getFromSWAPI() {
     // assign values to any necessary variables
     fetch(`https://swapi.dev/api/${queryType}/${itemID}`)
-    .then(function (response) {
-        return response.json()
-    })
-    .then(function(data){
-        updateInfo(data)
-    })
-    .catch(function(err) {
-        console.warn(err)
-    })
+        .then(response => response.json())
+        .then(data => updateInfo(data))
+        .catch(err => console.warn(err));
 }
+
 
 // create a new function called 'updateInfo()' that receives the data from 
 // the call to that function (see above). Use logic to write the appropriate
-//labels to 'dataLabel1' and 'dataLabel2' elements in starwars.html, as well
+// labels to 'dataLabel1' and 'dataLabel2' elements in starwars.html, as well
 // as the appropriate values from the data object to the 'dataValue1' and 
 // 'dataValue2' elements in starwars.html.
+
+function updateInfo(data) {
+    const keys = Object.keys(data);
+    document.querySelector("#dataLabel1").textContent = keys[0];
+    document.querySelector("#dataLabel2").innerText = keys[5];
+    document.querySelector("#dataValue1").innerText = data[keys[0]];
+    document.querySelector("#dataValue2").innerText = data[keys[5]];
+    checkQueryType()
+}
+
+function checkQueryType() {
+    if (queryType == "people") return true;
+
+    else return false
+}
